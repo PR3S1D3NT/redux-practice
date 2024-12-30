45538e16db21973272c37c6039a6b1f2381be33c
PR3S1D3NT <shinjitghosh@rediffmail.com>
2024-12-31 03:31:54 +0530

    Three Principles/Concepts Implementation

--- index.js
+++ index.js
@@ -1,2 +1,40 @@
-console.log("From index.js")
-console.log("Hello World")
+const redux = require ('redux');
+const createStore = redux.createStore;
+
+const initial_state = {
+    numberOfCookies : 10
+}
+
+//Action type
+const BUY_COOKIE = 'BUY_COOKIE';
+
+//Action creator
+const orderCookie = () =>{
+    return {
+        type: BUY_COOKIE
+    }
+}
+
+//Reducer
+const reducer = (state = initial_state ,action) => {
+    switch (action.type){
+        case BUY_COOKIE: {
+            return {
+                ...state,
+                numberOfCookies:state.numberOfCookies - 1,
+            }
+        }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer);
+
+console.log("Initial State",store.getState());
+const listener = store.subscribe(() => { console.log("Updated value ", store.getState())});
+console.log(store.dispatch(orderCookie()));
+console.log(store.dispatch(orderCookie()));
+console.log(store.dispatch(orderCookie()));
+console.log(store.dispatch(orderCookie()));
+listener();
